053e4a40dced2d0f1deec2ca654e9540ee3dd6de
Grant Oxer <grant.oxer@gmail.com>
2023-12-11 13:58:58 -0800

    added Score and Pin models to sspscoresapp

--- sspscoretrackersite/sspscoresapp/models.py
+++ sspscoretrackersite/sspscoresapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    score = models.IntegerField(default=0)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    pin = models.ForeignKey('Pin', on_delete=models.CASCADE)
+
+class Pin(models.Model):
+    name = models.CharField(max_length=200)
