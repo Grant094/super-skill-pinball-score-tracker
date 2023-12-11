dccac91e1012fe589f152c5cc03b60cdcadfc17b
Grant Oxer <grant.oxer@gmail.com>
2023-12-11 14:28:59 -0800

    added __str__() functions to Score and Pin models

--- sspscoretrackersite/sspscoresapp/models.py
+++ sspscoretrackersite/sspscoresapp/models.py
@@ -6,5 +6,11 @@ class Score(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     pin = models.ForeignKey('Pin', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.score) + " on " + str(self.pin) + " at " + str(self.timestamp)
+    
 class Pin(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
