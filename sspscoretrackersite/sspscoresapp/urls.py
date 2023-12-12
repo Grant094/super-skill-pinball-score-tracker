99d981a48a2fd47bdebe4d4ffcac0ecf63e1f981
Grant Oxer <grant.oxer@gmail.com>
2023-12-11 17:50:50 -0800

    changed sspscoresapp/urls.py to use views.ScoresListView instead of importing ScoresListView separately

--- sspscoretrackersite/sspscoresapp/urls.py
+++ sspscoretrackersite/sspscoresapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
 from . import views
-from .views import *
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("scores/", ScoresListView.as_view(), name="scores-list"),
+    path("scores/", views.ScoresListView.as_view(), name="scores-list"),
 ]
