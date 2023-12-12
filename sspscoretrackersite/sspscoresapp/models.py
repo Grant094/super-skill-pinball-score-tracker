297a34f1bb2038ddeaa8910940ea4530d402d5ec
Grant Oxer <grant.oxer@gmail.com>
2023-12-12 10:16:26 -0800

    added player field to Scores tied to django.contrib.auth.models.User model

--- sspscoretrackersite/sspscoresapp/models.py
+++ sspscoretrackersite/sspscoresapp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Score(models.Model):
     score = models.IntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
     pin = models.ForeignKey('Pin', on_delete=models.CASCADE)
+    player = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return str(self.score) + " on " + str(self.pin) + " at " + str(self.timestamp)
