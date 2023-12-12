b05adea42b883c7bf5346043ef8e4d0737ed1240
Grant Oxer <grant.oxer@gmail.com>
2023-12-11 17:48:04 -0800

    created scores_list.html at domain/scores/ and enabled listing recent scores

--- sspscoretrackersite/sspscoresapp/urls.py
+++ sspscoretrackersite/sspscoresapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import *
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("scores/", ScoresListView.as_view(), name="scores-list"),
 ]
--- sspscoretrackersite/sspscoresapp/views.py
+++ sspscoretrackersite/sspscoresapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import *
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hello world!')
+
+class ScoresListView(ListView):
+    model = Score
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
